166a54ab2378db687992e24c42f0ee2c71914fd5
tharupama <tharupama826@gmail.com>
2025-10-18 09:12:02 +0530

    update procedure attendance get by id as a summery of course unit

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,7 +1,8 @@
+DROP PROCEDURE IF EXISTS attendance_by_stid; 
 DELIMITER //
 
 CREATE PROCEDURE attendance_by_stid(IN id VARCHAR(20))
 BEGIN
-SELECT * FROM attendance WHERE ST_Id = id;
+SELECT Course_code, Session_Type,(SELECT COUNT(Status) FROM attendance WHERE Status = "Present" AND ST_Id = id) AS Present,(SELECT COUNT(Status) FROM attendance WHERE Status = "Absent" AND ST_Id = id) AS Absent FROM attendance WHERE ST_Id = id GROUP BY Course_code,Session_Type;
 END //
 DELIMITER ;
