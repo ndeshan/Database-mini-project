1f1d51fe57ab78da177950624465e2ceb03bcd13
tharupama <tharupama826@gmail.com>
2025-10-18 18:35:12 +0530

    create procedure attendance by atid and course code

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,15 +1,8 @@
-DROP PROCEDURE IF EXISTS attendance_by_stid;
+DROP PROCEDURE IF EXISTS attendance_by_stid_cid;
 DELIMITER //
-CREATE PROCEDURE attendance_by_stid(IN p_stid VARCHAR(20))
+CREATE PROCEDURE attendance_by_stid_cid(IN st_id VARCHAR(20), IN c_id VARCHAR(20))
 BEGIN
-  SELECT
-    Course_code,
-    Session_Type,
-    SUM(CASE WHEN Status = 'Present' THEN 1 ELSE 0 END) AS Present,
-    SUM(CASE WHEN Status = 'Absent'  THEN 1 ELSE 0 END) AS Absent,
-    COUNT(*) AS Total
-  FROM attendance
-  WHERE ST_Id = p_stid
-  GROUP BY Course_code, Session_Type;
+SELECT Session_Date,Week_Number,Session_Type,Status FROM attendance WHERE ST_Id = st_id AND 
+Course_code = c_id;   
 END //
 DELIMITER ;
