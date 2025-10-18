1d800de8131dd9978d67118ddeb030f08ffca0e0
tharupama <tharupama826@gmail.com>
2025-10-18 19:23:43 +0530

    create procedure to find if theory and practical both have in one subject find seperate procedure

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,8 +1,27 @@
-DROP PROCEDURE IF EXISTS attendance_by_stid_cid;
+DROP PROCEDURE IF EXISTS attendance_by_stid_cid_seperate_type;
 DELIMITER //
-CREATE PROCEDURE attendance_by_stid_cid(IN st_id VARCHAR(20), IN c_id VARCHAR(20))
+CREATE PROCEDURE attendance_by_stid_cid_seperate_type(
+	IN st_id VARCHAR(20),
+	IN c_id VARCHAR(20),
+	IN s_type VARCHAR(2)
+)
 BEGIN
-SELECT Session_Date,Week_Number,Session_Type,Status FROM attendance WHERE ST_Id = st_id AND 
-Course_code = c_id;   
+
+	DECLARE se_type VARCHAR(20);
+
+	IF s_type IN ('t','T') THEN
+		SET se_type = 'Theory';
+	ELSEIF s_type IN ('p','P') THEN
+		SET se_type = 'Practical';
+	ELSE
+		SELECT 'Invalid input please enter (\'t\',\'T\') or (\'p\',\'P\')' AS MESSAGE;
+		
+	END IF;
+
+	SELECT Session_Date, Week_Number, Session_Type, Status
+	FROM attendance
+	WHERE ST_Id = st_id
+	  AND Course_code = c_id
+	  AND Session_Type = se_type;
 END //
 DELIMITER ;
